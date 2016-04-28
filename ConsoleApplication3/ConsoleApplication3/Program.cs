d27ce8b4b28d8ea4c5c7c16ea678547d82a2e044
Michael Satran <michael.satran@outlook.com>
2016-04-27 23:36:23 -0700

    Added encryption

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -3,11 +3,104 @@ using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace ConsoleApplication3
 {
     public static class Program
     {
+        #region Encryption
+
+        private static int _iterations = 2;
+        private static int _keySize = 256;
+
+        private static string _hash = "SHA1";
+        private static string _salt = "aselrias38490a32";
+        private static string _vector = "8947az34awl34kjq";
+
+        private static string Encrypt(string value, string password)
+        {
+            return Encrypt<AesManaged>(value, password);
+        }
+        private static string Encrypt<T>(string value, string password) where T : SymmetricAlgorithm, new ()
+        {
+            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = UTF8Encoding.UTF8.GetBytes(value);
+
+            byte[] encrypted;
+
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
+                {
+                    using (MemoryStream to = new MemoryStream())
+                    {
+                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
+                        {
+                            writer.Write(valueBytes, 0, valueBytes.Length);
+                            writer.FlushFinalBlock();
+                            encrypted = to.ToArray();
+                        }
+                    }
+                }
+                cipher.Clear();
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string Decrypt(string value, string password)
+        {
+            return Decrypt<AesManaged>(value, password);
+        }
+        public static string Decrypt<T>(string value, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = Convert.FromBase64String(value);
+
+            byte[] decrypted;
+            int decryptedByteCount = 0;
+
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                try
+                {
+                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
+                    {
+                        using (MemoryStream from = new MemoryStream(valueBytes))
+                        {
+                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
+                            {
+                                decrypted = new byte[valueBytes.Length];
+                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return String.Empty;
+                }
+
+                cipher.Clear();
+            }
+            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
+        }
+
+
+        #endregion
 
         [STAThread]
         static void Main(string[] args)
@@ -16,8 +109,12 @@ namespace ConsoleApplication3
             {
                 Console.WriteLine("Error: Please use either -h to hide an image or -u to unhide an image.");
                 Console.WriteLine("Running in debug mode...");
-                string[] debugArgs = new string[] { "-h", @"C:\Users\misatran\OneDrive\Pictures\Engagement photos\Sharel and Michael-2.jpg" };
-                Run(debugArgs);
+                //string[] debugArgs = new string[] { "-h", @"C:\Users\misatran\OneDrive\Pictures\Engagement photos\Sharel and Michael-2.jpg" };
+                //Run(debugArgs);
+
+                string encrypted = Encrypt("Hello, worldasdfasdfasdfasdfasdfasdfasdfadf", "Mic1007805");
+                string decrypted = Decrypt("GZy4c9XEhi7F0h8+3HnI1A==", "Mic1007804");
+
             }
             else
             {
